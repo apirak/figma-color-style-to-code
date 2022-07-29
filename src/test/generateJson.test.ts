f129f77901c95f3f4f4f321b5b28c6ed96acdb62
Apirak Panatkool <apirakb@gmail.com>
2022-07-29 19:55:09 +0700

    add test for json and scss

---
+++ src/test/generateJson.test.ts
@@ -0,0 +1,31 @@
+// import { ColorStyle, BrandColorStyle } from "../utility/styleUtility";
+import { brandStyle, localStyle } from "./prepareDAta";
+import { createFigma } from "figma-api-stub";
+import { codeLocalStyle } from "../generateJson";
+
+describe("Separete Key from Name", () => {
+  // @ts-ignore
+  global.figma = createFigma({
+    simulateErrors: true,
+  });
+
+  it("generate style without brand color", () => {
+    let generatedCode = [
+      `"#f2994a":"fill_style_1",`,
+      `"#fccc0066":"fill_style_2",`,
+      `"#fccce6":"fill_style_3",`,
+      `"#00000000":"fill_Style_4"`
+    ];
+    expect(codeLocalStyle(localStyle, {})).toEqual(generatedCode.join("\n"));
+  });
+
+  it("generate style with brand color", () => {
+    let generatedCode = [
+      `"A__Color":"fill_style_1",`,
+      `"B__Color":"fill_style_2",`,
+      `"#fccce6":"fill_style_3",`,
+      `"Blank__Color":"fill_Style_4"`
+    ]
+    expect(codeLocalStyle(localStyle, brandStyle)).toEqual(generatedCode.join("\n"));
+  });
+});
---
+++ src/test/generateSCSS.test.ts
@@ -0,0 +1,31 @@
+// import { ColorStyle, BrandColorStyle } from "../utility/styleUtility";
+import { brandStyle, localStyle } from "./prepareDAta";
+import { createFigma } from "figma-api-stub";
+import { codeLocalStyle } from "../generateSCSS";
+
+describe("Separete Key from Name", () => {
+  // @ts-ignore
+  global.figma = createFigma({
+    simulateErrors: true,
+  });
+
+  it("generate style without brand color", () => {
+    let generatedCode = [
+      "$fill_style_1: #f2994a;",
+      "$fill_style_2: rgba(253, 204, 0, 0.4);",
+      "$fill_style_3: #fccce6;",
+      "$fill_Style_4: rgba(0, 0, 0, 0);"
+    ];
+    expect(codeLocalStyle(localStyle, {})).toEqual(generatedCode.join("\n"));
+  });
+
+  it("generate style with brand color", () => {
+    let generatedCode = [
+      "$fill_style_1: $A__Color",
+      "$fill_style_2: $B__Color",
+      "$fill_style_3: #fccce6",
+      "$fill_Style_4: $Blank__Color"
+    ]
+    expect(codeLocalStyle(localStyle, brandStyle)).toEqual(generatedCode.join("\n"));
+  });
+});
